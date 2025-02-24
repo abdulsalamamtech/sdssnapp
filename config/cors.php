f48282fd80408ed0a65df2bb5d1b10c54541f023
abdulsalamamtech <abdulsalamamtech@gmail.com>
2025-02-24 23:57:14 +0100

    Add api github workfloe

--- config/cors.php
+++ config/cors.php
@@ -19,6 +19,7 @@ return [
     'paths' => ['*', 'api/*', 'sanctum/csrf-cookie'],
 
     'allowed_methods' => ['*'],
+    // 'allowed_methods' => ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
 
     // 'allowed_origins' => ['*'],
     'allowed_origins' => [
