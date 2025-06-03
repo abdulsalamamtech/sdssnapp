a34c35852827b5b0e757ab6d1966171ccd03eaed
abdulsalamamtech <abdulsalamamtech@gmail.com>
2025-06-03 22:00:31 +0100

    Modified cert request migration

--- database/migrations/2025_06_03_201843_create_certification_requests_table.php
+++ database/migrations/2025_06_03_201843_create_certification_requests_table.php
@@ -12,8 +12,24 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('certification_requests', function (Blueprint $table) {
+            // user_id
+            // certification_id [from UI]
+            // full_name [from user account]
+            // signature_image_id (from user) [from UI]
+            // reason_for_certification [text editor] [from UI]
+            // management_note 
+            // status (pending, rejected, approved, paid)
             $table->id();
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('certification_id')->constrained('certifications')->onDelete('cascade');
+            $table->string('full_name');
+            $table->foreignId('user_signature_id')->constrained('certifications')->onDelete('cascade');
+            $table->foreignId('credential_id')->constrained('credentials')->onDelete('cascade');
+            $table->text('reason_for_certification')->nullable();
+            $table->text('management_note')->nullable();
+            $table->enum('status', ['pending', 'rejected', 'approved', 'paid'])->default('pending');
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
