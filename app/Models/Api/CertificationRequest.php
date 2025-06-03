8a8618d489cada0f138e4fbe10851b2a8b590d50
abdulsalamamtech <abdulsalamamtech@gmail.com>
2025-06-03 22:00:48 +0100

    Modified model

--- app/Models/Api/CertificationRequest.php
+++ app/Models/Api/CertificationRequest.php
@@ -3,8 +3,20 @@
 namespace App\Models\Api;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class CertificationRequest extends Model
 {
-    //
+    use SoftDeletes;
+
+    protected $fillable = [
+        'user_id',
+        'certification_id',
+        'full_name',
+        'user_signature_id',
+        'reason_for_certification',
+        'management_note',
+        'credential_id',
+        'status',
+    ];
 }
