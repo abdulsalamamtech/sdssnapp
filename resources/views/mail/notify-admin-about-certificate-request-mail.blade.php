151dc1ceb3ddd0d08d92bb89a14f13879cb1eb05
abdulsalamamtech <abdulsalamamtech@gmail.com>
2025-06-20 18:28:28 +0100

    Modified file

--- resources/views/mail/notify-admin-about-certificate-request-mail.blade.php
+++ resources/views/mail/notify-admin-about-certificate-request-mail.blade.php
@@ -1,19 +1,23 @@
-<div>
-    <!-- Waste no more time arguing what a good man should be, be one. - Marcus Aurelius -->
-    Hello Admin
-    <p>
-        {{-- {{ json_encode($certificationRequest) }} has requested for certification. --}}
-        {{ $certificationRequest?->full_name ?? 'A user' }} has requested for certification.
+@extends('layouts.mail')
 
-    </p>
-    {{-- reason_for_certification --}}
-    <p>
-        <strong>Reason for applying for certification:</strong>
-        <br>
-        {{ $certificationRequest?->reason_for_certification }}
-    </p>
+@section('content')
+    <div>
+        <!-- Waste no more time arguing what a good man should be, be one. - Marcus Aurelius -->
+        Hello Admin
+        <p>
+            {{-- {{ json_encode($certificationRequest) }} has requested for certification. --}}
+            {{ $certificationRequest?->full_name ?? 'A user' }} has requested for certification.
 
-    Regards,<br>
-    {{ config('app.name') ?? 'SDSSN' }} <br>
-    {{ config('app.frontend_url') ?? 'sdssn.org' }} <br>
-</div>
+        </p>
+        {{-- reason_for_certification --}}
+        <p>
+            <strong>Reason for applying for certification:</strong>
+            <br>
+            {{ $certificationRequest?->reason_for_certification }}
+        </p>
+
+        {{-- Regards,<br>
+        {{ config('app.name') ?? 'SDSSN' }} <br>
+        {{ config('app.frontend_url') ?? 'sdssn.org' }} <br> --}}
+    </div>
+@endsection
