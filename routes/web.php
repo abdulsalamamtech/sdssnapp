b9ef454ba904ed1e5940de80c0915657962b7458
abdulsalamamtech <abdulsalamamtech@gmail.com>
2025-06-20 13:07:56 +0100

    Modified route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-
+use App\Mail\CertificationRequestRejectedMail;
 use App\Mail\NotifyAdminAboutCertificateRequestMail;
 use App\Models\Api\CertificationRequest;
 use App\Models\Api\Membership;
@@ -25,13 +25,16 @@ require __DIR__ . '/auth.php';
 
 Route::get('/mail', function (Request $request) {
 
-    // $certificationRequest = CertificationRequest::find(1); // Assuming you have a certification request with ID 1
+    $certificationRequest = CertificationRequest::find(1); // Assuming you have a certification request with ID 1
     // Mail::to('abdulsalamamtech@gmail.com')->send(new NotifyAdminAboutCertificateRequestMail($certificationRequest));
+    Mail::to('abdulsalamamtech@gmail.com')->send(new CertificationRequestRejectedMail($certificationRequest));
 
-    $send = Mail::raw('This is a test email, from: SDSSN', function ($message) {
-        $message->to('abdulsalamamtech@gmail.com')->subject('Test Email: ' . now());
-    });
-    return $send ? "done" : "fail";
+    return "DONE";
+
+    // $send = Mail::raw('This is a test email, from: SDSSN', function ($message) {
+    //     $message->to('abdulsalamamtech@gmail.com')->subject('Test Email: ' . now());
+    // });
+    // return $send ? "done" : "fail";
 });
 
 // /home/amtech/Desktop/projects/sdssnapp/resources/views/custom/tawk/index.blade.php
