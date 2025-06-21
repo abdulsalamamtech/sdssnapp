de419228a00ef5b3d43460a7eb87430c0fe72384
abdulsalamamtech <abdulsalamamtech@gmail.com>
2025-06-21 16:07:56 +0100

    Add cert request submitted mail view

---
+++ resources/views/mail/certification-request-submitted-mail.blade.php
@@ -0,0 +1,31 @@
+{{-- <div>
+    <!-- Simplicity is the ultimate sophistication. - Leonardo da Vinci -->
+</div> --}}
+
+@extends('layouts.mail')
+
+@section('content')
+
+
+        <!-- Salutation -->
+        <h2>Dear {{ $certificationRequest?->full_name }},</h2>
+        <p>
+            Thanks for submitting your certification request! for 
+            {{ $certificationRequest?->certification->title }}
+            {{ $certificationRequest?->certification->type }}
+            certification.
+        </p>
+        <p>
+             We want to let you know that we've received it and will be reviewing it within 3 working days.
+        </p>
+        <p>
+            You'll get another email from us once the review is complete. 
+            In the meantime, you can always check the status of 
+            your request on your dashboard.
+        </p>
+        <p>
+            Thanks for your patience!
+        </p>
+
+@endsection
+
