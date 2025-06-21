d6a008a57ecde83cb0d2f27dc2e500d0a6259b9c
abdulsalamamtech <abdulsalamamtech@gmail.com>
2025-06-21 16:07:37 +0100

    Add cert request submitted mail

---
+++ app/Mail/CertificationRequestSubmittedMail.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class CertificationRequestSubmittedMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * The certification request instance.
+     *
+     * @var mixed
+     */
+    public $certificationRequest;
+
+    /**
+     * Create a new message instance.
+     */
+    public function __construct($certificationRequest)
+    {
+        $this->certificationRequest = $certificationRequest;
+    }
+
+
+    /**
+     * Get the message envelope.
+     */
+    public function envelope(): Envelope
+    {
+        return new Envelope(
+            subject: 'Certification Request Submitted',
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     */
+    public function content(): Content
+    {
+        info('Sending email to user about certification request submitted: ' . $this->certificationRequest->id);
+        info('Certification Request Details: ', [
+            'email' => $this->certificationRequest->user->email,
+            'user_id' => $this->certificationRequest->user_id,
+            'full_name' => $this->certificationRequest->full_name,
+            'status' => $this->certificationRequest->status,
+        ]);
+        return new Content(
+            view: 'mail.certification-request-submitted-mail',
+            with: [
+                'certificationRequest' => $this->certificationRequest,
+            ],
+        );
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array<int, \Illuminate\Mail\Mailables\Attachment>
+     */
+    public function attachments(): array
+    {
+        return [];
+    }
+}
