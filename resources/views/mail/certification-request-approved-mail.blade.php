1ef108c9285c1ec3cfcf4cebccc606fe30fb5402
abdulsalamamtech <abdulsalamamtech@gmail.com>
2025-06-21 14:45:09 +0100

    Add cert request approved mail view

---
+++ resources/views/mail/certification-request-approved-mail.blade.php
@@ -0,0 +1,36 @@
+{{-- <div>
+    <!-- It is never too late to be what you might have been. - George Eliot -->
+</div> --}}
+
+@extends('layouts.mail')
+
+@section('content')
+
+
+        <!-- Salutation -->
+        <h2>Dear {{ $certificationRequest?->full_name }},</h2>
+        <p>
+            Thank you for your application for the
+            {{ $certificationRequest?->certification->title }}
+            {{ $certificationRequest?->certification->type }}
+            certification.
+        </p>
+        <p>
+            After review, we regret to inform you that we are unable
+            to approve your certification at this time.
+            Your application did not meet the necessary requirements.
+        </p>
+        <p>
+            {{-- quoted word --}}
+        <blockquote>
+            {{ $certificationRequest?->management_note }}
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit tempore ratione consequatur, exercitationem
+            quidem saepe culpa mollitia cum dolorem vel.
+        </blockquote>
+        </p>
+        <p>
+            We encourage you to review our certification requirements and consider reapplying.
+        </p>
+
+@endsection
+
