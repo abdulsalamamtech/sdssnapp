3dce061cb469e44db6a5a7fcdb71e0325cc14db2
abdulsalamamtech <abdulsalamamtech@gmail.com>
2026-01-17 22:54:27 +0100

    feat (db): modified admin seeder

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -13,26 +13,29 @@ class AdminSeeder extends Seeder
      */
     public function run(): void
     {
-                // Application admin
-        if (!User::where('email', 'info@sdssn.org')->exists()) {
-            $user = User::create([
-                'name' => 'Admin User',
-                'email' => 'info@sdssn.org',
-                'password' => bcrypt('password'), // Use bcrypt for password hashing by default
-                'email_verified_at' => now(),
-                'first_name' => 'Admin',
-                'last_name' => 'User',
-                'security_question' => 'What is your favorite color?',
-                'answer' => 'Blue',
-                'phone_number' => '1234567890',
-                'address' => '123 Admin St, Admin City, Admin Country',
-                'state' => 'Admin State',
-                'country' => 'Admin Country',
-            ]);
-            $user->assignRole('super-admin');
-            $user->assignRole('admin');
-
-            info('Admin user created with email: info@sdssn.org');
+        // Application admin
+        $adminEmail = ['info@sdssn.org'];
+        foreach($adminEmail as $email){
+            if (!User::where('email', $email)->exists()) {
+                $user = User::create([
+                    'name' => 'Admin User',
+                    'email' => $email,
+                    'password' => bcrypt('password'), // Use bcrypt for password hashing by default
+                    'email_verified_at' => now(),
+                    'first_name' => 'Admin',
+                    'last_name' => 'User',
+                    'security_question' => 'What is your favorite color?',
+                    'answer' => 'Blue',
+                    'phone_number' => '1234567890',
+                    'address' => '123 Admin St, Admin City, Admin Country',
+                    'state' => 'Admin State',
+                    'country' => 'Admin Country',
+                ]);
+                $user->assignRole('super-admin');
+                $user->assignRole('admin');
+    
+                info('Admin user created with email: ' . $email);
+            }
         }
     }
 }
