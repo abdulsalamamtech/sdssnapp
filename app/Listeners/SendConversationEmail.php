8b4a7482f5d8eac4372ba4ea702075922f3ad2cc
abdulsalamamtech <abdulsalamamtech@gmail.com>
2026-04-09 12:33:02 +0100

    feat (listeners): modified file

--- app/Listeners/SendConversationEmail.php
+++ app/Listeners/SendConversationEmail.php
@@ -26,16 +26,14 @@ class SendConversationEmail
      */
     public function handle(ConversationSaved $event): void
     {
-        // Access the user using $event->user...
-        $users = $event->conversation->sentTo();
         $content['subject'] = $event->conversation->subject;
         $content['message'] = $event->conversation->message;
         $sendToUsers = $event->conversation->sentTo();
         Log::info('Users to send email to: ',  [$sendToUsers]);
 
-        foreach ($users as $user) {
+        foreach ($sendToUsers as $user) {
             Mail::to($user?->email)->send(new ConversationMail($user, $content));
-            info('Conversation: ' . $event->conversation->sentBy->email . ' Sent: ' . $event->conversation->reason . ' To: ' . $user);
+            Log::info('Conversation - Email sent by: ' . $event->conversation->sentBy->email . ' Sent to: ' . $user->email);
         }
 
         // update the conversation record to indicate that the email has been sent
